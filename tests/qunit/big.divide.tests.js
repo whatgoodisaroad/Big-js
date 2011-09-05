a8c4e8ed1a4e2db12133d1c2f9c274b9d565a53d
Wyatt Allen <wyatt.allen0@gmail.com>
2011-09-04 18:44:23 -0700

    Changed accuracy in unit test

--- tests/qunit/big.divide.tests.js
+++ tests/qunit/big.divide.tests.js
@@ -28,8 +28,8 @@ test("divide(l, r)", function() {
     
     l = new Big(2.6);
     r = new Big(-2.85);
-    expected = new Big("-0.91228070175438596491");
+    expected = new Big("-0.912280701754385964");
     result = normalize(divide(l, r));
-    deepEqual(result + "", expected + "", "divide correctly finds quotient reals");
+    equal(result + "", expected + "", "divide correctly finds quotient reals");
     deepEqual(result.exponent, expected.exponent, "divide correctly finds quotient exponent");
 });
